4f2518eb31e1dc4568e541d28a46705da6f22899
Giacomo Torrigiani <101568206+GiacomoTorrigiani@users.noreply.github.com>
2023-10-24 22:44:32 +0200

    Add files via upload

---
+++ YT/main.py
@@ -0,0 +1,45 @@
+import telegram.ext
+from telegram.ext import CommandHandler, MessageHandler, Filters, Updater
+from dotenv import load_dotenv
+import os
+from pytube import YouTube
+
+load_dotenv()
+TOKEN = os.getenv("TOKEN")
+
+def start(update, context):
+    update.message.reply_text("Benvenuto in questo BOT.")
+    update.message.reply_text("Scrivi l'URL di un video di YouTube:")
+
+def handle_input(update, context):
+    url = update.message.text
+
+    # Puoi fare ciò che vuoi con il valore immesso, ad esempio assegnarlo a una variabile
+    yt = YouTube(url)
+    
+    # Scegli il formato MP4 in streaming
+    vid = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first()
+    
+    # Imposta il percorso di download
+    download_path = 'percorso/del/tuo/download'  # Sostituisci con il percorso desiderato
+
+    # Scarica il video
+    video_path = vid.download(output_path=download_path)
+
+    update.message.reply_text("Video in anteprima:")
+
+    # Invia il video scaricato come messaggio video
+    context.bot.send_video(chat_id=update.effective_chat.id, video=open(video_path, 'rb'))
+
+    # Elimina il file locale
+    os.remove(video_path)
+
+if __name__ == "__main__":
+    updater = Updater(TOKEN, use_context=True)
+    dp = updater.dispatcher
+
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(MessageHandler(Filters.text & ~Filters.command, handle_input))
+
+    updater.start_polling()
+    updater.idle()
